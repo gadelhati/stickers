af54dbbb67c7b4d4cdb081afeb020f42b5d186e1
gadelhati <gadelha.ti@gmail.com>
2026-06-24 20:21:36 -0300

    feat: add year on sticker back

--- src/component/team.ts
+++ src/component/team.ts
@@ -1,16 +1,24 @@
 export interface Team {
     readonly id: string,
     name: string,
+    colorOne: string,
+    colorTwo: string,
 }
 export interface TeamValidation {
     readonly id: string,
     readonly name: string,
+    readonly colorOne: string,
+    readonly colorTwo: string,
 }
 export const initialTeamValidation: TeamValidation = {
     id: `^[a-zA-Z0-9]+$`,
     name: `^[a-zA-Z0-9]+$`,
+    colorOne: `^#[0-9A-Fa-f]{6}$`,
+    colorTwo: `^#[0-9A-Fa-f]{6}$`,
 }
 export const initialTeam: Team = {
     id: '001',
     name: 'Flamengo',
+    colorOne: '#449966',
+    colorTwo: '#F9DF43',
 }
--- src/page/sticker.tsx
+++ src/page/sticker.tsx
@@ -1,9 +1,7 @@
 import { useState } from 'react';
 import { initialCharacter, type Character } from '../component/character';
 import cup from '../assets/logo-cup-white.png';
-import two from '../assets/twoVector.png';
-import six from '../assets/six.svg';
-// import photo from '../assets/photo.png';
+import photo from '../assets/photo.png';
 import uniform from '../assets/uniform.png';
 import panini from '../assets/panini-logo.svg';
 import flag from '../assets/flagBrazil.webp';
@@ -26,8 +24,14 @@ export const StickerPage = () => {
             <header><img src={cup} alt="Taça 2026" /></header>
 
             <center className="photo">
-               <img className="two year" src={two} alt="Two of Year" />
-               <img className="six year" src={two} alt="Six of Year" />
+               <svg className="six year" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 327.24 412.75 309.56">
+                  <path fill={character.team.colorTwo} d="M309.56 327.24H103.19C46.2 327.24 0 373.44 0 430.43v103.19c0 56.99 46.2 103.19 103.19 103.19h206.37c56.99 0 103.19-46.2 103.19-103.19s-46.2-103.19-103.19-103.19h103.19c0-56.99-46.2-103.19-103.19-103.19"/>
+               </svg>
+               <svg className="two year" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 412.75 309.56" >
+                  <path fill={character.team.colorOne} d="M309.56 0H103.19C46.2 0 0 46.2 0 103.19h103.19C46.2 103.19 0 149.38 0 206.37v103.19h412.75V206.37H309.56c56.99 0 103.19-46.2 103.19-103.19S366.55 0 309.56 0"/>
+               </svg>
+               <img src={photo} alt="Photo" />
+               <img src={uniform} alt="Uniform" />
             </center>
 
             <footer>
